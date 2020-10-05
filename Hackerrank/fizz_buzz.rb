1afd799266a75e63910ece1c4b1817fd7e1cca2a
Sajjad Ahmad <jogimar14@gmail.com>
2020-10-05 17:42:11 +0500

    add solution

---
+++ Hackerrank/fizz_buzz.rb
@@ -0,0 +1,15 @@
+def fizz_buzz
+  100.times do |i|
+    i+=1
+      if i%15==0
+        puts "FizzBuzz"
+      elsif i%5==0
+        puts "Buzz"
+      elsif i%3==0
+        puts "Fizz"
+      else
+        puts i
+      end
+    end
+end
+fizz_buzz()
