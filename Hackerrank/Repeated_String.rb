944731b3315080fe9ca324a0fb5194f0724e9f80
Sajjad Ahmad <jogimar14@gmail.com>
2020-10-05 13:05:39 +0500

    made solution effiecient

--- Hackerrank/Repeated_String.rb
+++ Hackerrank/Repeated_String.rb
@@ -6,30 +6,19 @@ def repeatedString(s, n)
   s_size = s.size
   count_a = 0
   occurence_a = 0
-  s_size.times do |i| 
-    if s[i] == 'a'
-      count_a += 1
-    end
-  end
+  count_a = s.count('a')
   if s_size >= n
-    s[0..n-1].size.times do |i|
-      if s[i] == 'a'
-        occurence_a += 1
-      end
-    end
+    occurence_a = s[0..n-1].count('a')
   else
     diff = n - s_size
     multiple = diff / s_size
     remain = diff % s_size
     if remain > 0
-      s[0..remain-1].size.times do |i|
-        if s[i] == 'a'
-          occurence_a += 1
-        end
-      end
+      occurence_a = s[0..remain-1].count('a')
     end
     occurence_a += count_a + count_a * multiple
   end
   occurence_a
 end
-p repeatedString('aba', 10)
+
+p repeatedString('aba', 4)
