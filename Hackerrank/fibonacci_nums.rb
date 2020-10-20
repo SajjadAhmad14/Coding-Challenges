44c6164b079f7b260a3beaba690255820eeba0b6
Sajjad Ahmad <jogimar14@gmail.com>
2020-10-20 12:31:02 +0500

    reduce complexity

--- Hackerrank/fibonacci_nums.rb
+++ Hackerrank/fibonacci_nums.rb
@@ -1,17 +1,15 @@
 
 def func(n)
   arr = [1, 2]
-  sum = 0
+  sum = 2
   last_num = arr[-1]+arr[-2]
   while last_num + arr[-2]<=n do
     last_num = arr[-1]+arr[-2]
     arr.push(last_num)
-  end
-  arr.size.times do |i|
-    if arr[i].even?
-      sum += arr[i]
+    if last_num.even?
+      sum += last_num
     end
   end
   sum
 end
-p func(20)
+p func(10)
