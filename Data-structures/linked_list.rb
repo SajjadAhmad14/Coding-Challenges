cfc530ca7a5cd7d5ab265bf25cabbc8ba14ec41e
Sajjad Ahmad <jogimar14@gmail.com>
2020-11-09 16:03:44 +0500

    create a new linkedlist, add append, prepend and at methods and define size of list

---
+++ Data-structures/linked_list.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+class Node
+  attr_accessor :value, :next_node
+
+  def initialize(value, next_node = nil)
+    @value = value
+    @next_node = next_node
+  end
+end
+
+class LinkedList
+  attr_reader :size, :head, :tail
+  def initialize
+    @head = nil
+    @tail = nil
+    @size = 0
+  end
+
+  def append(number)
+    new_node = Node.new(number)
+    if @head.nil?
+      @head = new_node
+      @tail = new_node
+    else
+      @tail.next_node = new_node
+      @tail = new_node
+    end
+    @size += 1
+  end
+
+  def at(position)
+    node = @head
+    node_index = 0
+    until node_index == position || node.next_node.nil?
+      node = node.next_node
+      node_index += 1
+    end
+    node.value
+  end
+
+  def prepend(number)
+    node = Node.new(number)
+    if @head.nil?
+      @head = node
+      @tail = node
+    else
+      node.next_node = @head
+      @head = node
+      @size += 1
+    end
+  end
+end
+list = LinkedList.new
+list.append(0)
+list.append(1)
+list.append(2)
+list.append(5)
+list.append(9)
+p list.size
