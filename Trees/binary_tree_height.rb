b27f6159b51c1374da6a98c87dc90c762940be58
Sajjad Ahmad <jogimar14@gmail.com>
2021-05-07 15:02:27 +0500

    Use recursion to calculate height of a binary tree

--- Trees/binary_tree_height.rb
+++ Trees/binary_tree_height.rb
@@ -1,13 +1,37 @@
-def binary_tree_height(arr)
-  left_height = 0;
-  right_height = 0;
-  count = 0;
-  while left_height < arr.length && right_height < arr.length
-    left_height = 2 * left_height + 1
-    right_height = 2 * right_height + 2
-    count += 1
-  end
-  count
+class Node
+  attr_reader :data
+  attr_accessor :left, :right
+
+  def initialize(data)
+    @data = data
+  end  
 end
 
-p binary_tree_height([2, 7, 5, 2, 6, 0, 9])
+def arr_to_tree(arr, i)
+  return nil if i >= arr.length || arr[i] == 0
+
+  node = Node.new(arr[i])
+  node.left = arr_to_tree(arr, 2*i+1)
+  node.right = arr_to_tree(arr, 2*i+2)
+  
+  node
+end
+
+def binary_tree_height(arr)
+  node = arr_to_tree(arr, 0);
+  height(node)
+end
+
+def height(node)
+  return 0 if node.nil?
+  left = height(node.left)
+  right = height(node.right)
+  max_height = max(left, right);
+  height = max_height + 1
+end
+
+def max(a, b)
+  a > b ? a : b
+end
+arr = [2, 7, 5, 2, 6, 0, 9]
+p binary_tree_height(arr)
